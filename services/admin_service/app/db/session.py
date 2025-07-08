3b8aad8713546664e48ecd8fdd5d0212384d1773
Anton <annnt9na@yandex.ru>
2025-07-08 18:09:34 +0300

    Created db & alembic migration

---
+++ services/admin_service/app/db/session.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# не создаём engine сразу — пусть делает env.py
+def get_async_sessionmaker():
+    from sqlalchemy.ext.asyncio import create_async_engine
+    engine = create_async_engine(DATABASE_URL, echo=True)
+    return sessionmaker(bind=engine, class_=AsyncSession, autoflush=False, autocommit=False)
